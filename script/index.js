883b96b7c68844e4811414fac35baf31636c6b94
Daria Matveeva <co-email@yandex.ru>
2023-02-12 01:32:35 +0300

    Feat: JS script for popup and input

---
+++ script/index.js
@@ -0,0 +1,49 @@
+const editPopup = document.querySelector('.popup');
+const profileButtonEdit = document.querySelector('.profile__button-edit');
+
+profileButtonEdit.addEventListener('click', function () {
+  const editPopup = document.querySelector('.popup');
+  editPopup.classList.remove('popup_hidden');
+});
+
+const popupCloseButton = document.querySelector('.popup__button-close');
+popupCloseButton.addEventListener('click', function () {
+  editPopup.classList.add('popup_hidden');
+});
+
+let user = {
+  name: 'Жак-Ив Кусто',
+  position: 'Исследователь океана'
+};
+
+const userName = document.querySelector('.profile__name');
+userName.textContent = user.name;
+
+const userPosition = document.querySelector('.profile__position');
+userPosition.textContent = user.position;
+
+const userNameInput = document.querySelector('.popup__input-name_type_name');
+userNameInput.value = user.name;
+
+const userPositionInput = document.querySelector('.popup__input-name_type_position');
+userPositionInput.value = user.position;
+
+const savePopupButton = document.querySelector('.popup__button-save');
+
+savePopupButton.addEventListener('click', function (event) {
+  if (!userPositionInput.value) {
+    throw new Error('Имя не введено!');
+  } else {
+    const value = userNameInput.value;
+    userName.textContent = value;
+  }
+});
+
+savePopupButton.addEventListener('click', function (event) {
+  if (!userPositionInput.value) {
+    throw new Error('Должность не введена!');
+  } else {
+    const value = userPositionInput.value;
+    userPosition.textContent = value;
+  }
+});
