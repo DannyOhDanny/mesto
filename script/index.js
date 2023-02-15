a5d45c6b22fc065c4406d4767f9dc7a99aebbb49
Daria Matveeva <co-email@yandex.ru>
2023-02-15 23:21:09 +0300

    Fix: review comments

--- script/index.js
+++ script/index.js
@@ -1,66 +1,29 @@
-const editPopup = document.querySelector('.popup');
+const popup = document.querySelector('.popup');
 const profileButtonEdit = document.querySelector('.profile__button-edit');
+const popupButtonClose = document.querySelector('.popup__button-close');
+const formElement = document.querySelector('.popup__form');
+let nameHTML = document.querySelector('.profile__name');
+let positionHTML = document.querySelector('.profile__position');
+let userNameInput = document.querySelector('.popup__input-name_type_name');
+let userPositionInput = document.querySelector('.popup__input-name_type_position');
 
-profileButtonEdit.addEventListener('click', function () {
-  const editPopup = document.querySelector('.popup');
-  editPopup.classList.remove('popup_hidden');
-});
+profileButtonEdit.addEventListener('click', openPopup);
+popupButtonClose.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handleFormSubmit);
 
-const popupCloseButton = document.querySelector('.popup__button-close');
-popupCloseButton.addEventListener('click', function () {
-  editPopup.classList.add('popup_hidden');
-});
+function closePopup() {
+  popup.classList.add('popup_hidden');
+}
 
-let user = {
-  name: 'Жак-Ив Кусто',
-  position: 'Исследователь океана'
-};
+function openPopup() {
+  popup.classList.remove('popup_hidden');
+  userNameInput.value = nameHTML.textContent;
+  userPositionInput.value = positionHTML.textContent;
+}
 
-const userName = document.querySelector('.profile__name');
-userName.textContent = user.name;
-
-const userPosition = document.querySelector('.profile__position');
-userPosition.textContent = user.position;
-
-const userNameInput = document.querySelector('.popup__input-name_type_name');
-userNameInput.value = user.name;
-
-const userPositionInput = document.querySelector('.popup__input-name_type_position');
-userPositionInput.value = user.position;
-
-const savePopupButton = document.querySelector('.popup__button-save');
-
-/* Предыдущий вариант без выхода из окна после ввода данных
-
-savePopupButton.addEventListener('click', function (event) {
-  if (!userPositionInput.value) {
-    throw new Error('Имя не введено!');
-  } else {
-    const value = userNameInput.value;
-    userName.textContent = value;
-  }
-});
-
-savePopupButton.addEventListener('click', function (event) {
-  if (!userPositionInput.value) {
-    throw new Error('Должность не введена!');
-  } else {
-    const value = userPositionInput.value;
-    userPosition.textContent = value;
-  }
-});
-*/
-
-savePopupButton.addEventListener('click', function (event) {
-  if (!userNameInput.value || !userPositionInput.value) {
-    throw new Error('Данные не введены!');
-  } else {
-    const nameValue = userNameInput.value;
-    userName.textContent = nameValue;
-
-    const positionValue = userPositionInput.value;
-    userPosition.textContent = positionValue;
-
-    editPopup.classList.add('popup_hidden');
-  }
-});
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  nameHTML.textContent = userNameInput.value;
+  positionHTML.textContent = userPositionInput.value;
+  closePopup();
+}
