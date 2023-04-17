5d35a8ae550afdec7136e607fb3918f9c7ec1784
Daria Matveeva <co-email@yandex.ru>
2023-04-18 00:31:29 +0300

    Main: constants dir fixed

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
 // Импорт стилей CSS
 import './index.css';
 
+//Импорт констант
+import {
+  initialCards,
+  settings,
+  initialCards,
+  elementsContainer,
+  profileButtonAdd,
+  profileButtonEdit,
+  userNameInput,
+  userPositionInput,
+  titleInput,
+  urlInput,
+  cardSection
+} from '../utils/constants.js';
+
 //Импорт классов в файл index.js
 import Card from '../script/Card.js';
 import Section from '../script/Section.js';
@@ -8,173 +23,6 @@ import FormValidator from '../script/FormValidator.js';
 import PopupWithImage from '../script/PopupWithImage.js';
 import PopupWithForm from '../script/PopupWithForm.js';
 import UserInfo from '../script/UserInfo.js';
-
-// Импорт картинок
-const solnechnogorsk = new URL('../images/element_pic-4.jpg', import.meta.url);
-const kurshskayaKosa = new URL('../images/element_pic-5.jpg', import.meta.url);
-const vyborg = new URL('../images/element_pic-6.jpg', import.meta.url);
-
-//Исходный массив
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  },
-  {
-    name: 'Солнечногорск',
-    link: solnechnogorsk
-  },
-  {
-    name: 'Куршская коса',
-    link: kurshskayaKosa
-  },
-  {
-    name: 'Выборг',
-    link: vyborg
-  }
-];
-
-//Объект - конфигуратор со свойствами селекторов и классов
-const settings = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button',
-  inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
-};
-
-//Константы
-const elementsContainer = document.querySelector('.elements');
-const profileButtonEdit = document.querySelector('.profile__button-edit');
-const profileButtonAdd = document.querySelector('.profile__button-add');
-const userNameInput = document.querySelector('.popup__input_type_name');
-const userPositionInput = document.querySelector('.popup__input_type_position');
-const titleInput = document.querySelector('.popup__input_type_heading');
-const urlInput = document.querySelector('.popup__input_type_url');
-const cardSection = '.elements';
-
-/*//Слушатели на формы
-addCardForm.addEventListener('submit', handleAddCardForm);
-editProfileForm.addEventListener('submit', handleProfileFormSubmit);
-profileButtonAdd.addEventListener('click', function () {
-  openPopup(addCardPopup);
-  addCardFormPopup.resetValidation();
-});*/
-
-/*//Функция редактирования профиля
-function handleProfileFormSubmit(evt) {
-evt.preventDefault();
-nameHTML.textContent = userNameInput.value;
-positionHTML.textContent = userPositionInput.value;
-}*/
-
-/*//Передача инфо из импута в карточку добавления изображения
-function handleAddCardForm(evt) {
-  evt.preventDefault();
-  const element = {
-    name: titleInput.value,
-    link: urlInput.value
-  };
-  addCard(element);
-  evt.target.reset();
-  closePopup(addCardPopup);
-}*/
-
-/*//Код, собирающий карточку:
-function getCard(item) {
-  const itemTemplate = document.querySelector('#element-template').content;
-  const cardItem = itemTemplate.querySelector('.element').cloneNode(true);
-  const cardPicture = cardItem.querySelector('.element__pic');
-  const deleteButton = cardItem.querySelector('.element__delete-btn');
-  const likeButton = cardItem.querySelector('.element__heart');
-  const cardHeading = cardItem.querySelector('.element__title');
-
-  cardPicture.addEventListener('click', cardItem => handleImgPopup(cardItem));
-
-  deleteButton.addEventListener('click', function (evt) {
-    cardItem.remove();
-  });
-
-  likeButton.addEventListener('click', function (event) {
-    event.target.classList.toggle('element__heart_active');
-  });
-
-  cardHeading.textContent = item.name;
-  cardPicture.setAttribute('src', item.link);
-  cardPicture.setAttribute('alt', item.name);
-
-  return cardItem;
-}*/
-
-/*//Вызов функции, создающей карточки из массива
-initialCards.forEach(createCard);*/
-
-/*//Присовение значений попапу изображения
-function handleImgPopup(evt) {
-  openPopup(imagePopup);
-  modalImg.src = evt.target.src;
-  modalImg.alt = evt.target.alt;
-  modalCaption.textContent = evt.target.alt;
-}*/
-
-/*//Функция открытия попапа
-function openPopup(popup) {
-  popup.classList.add('popup_opened');
-  document.addEventListener('keydown', closeByEscape);
-}*/
-
-/* //Функция закрытия попапа
-function closePopup(popup) {
-  popup.classList.remove('popup_opened');
-  document.removeEventListener('keydown', closeByEscape);
-}*/
-
-/*//Закрытие каждого попапа по нажатию на фон попапа и на кнопку-крестик
-popupWindows.forEach(popup => {
-  popup.addEventListener('mousedown', evt => {
-    if (evt.target.classList.contains('popup_opened')) {
-      closePopup(popup);
-    }
-    if (evt.target.classList.contains('popup__button-close')) {
-      closePopup(popup);
-    }
-  });
-});*/
-
-/*//Закрытие каждого попапа по нажатию на Esc
-function closeByEscape(evt) {
-  if (evt.key === 'Escape') {
-    const openedPopup = document.querySelector('.popup_opened');
-    closePopup(openedPopup);
-  }
-}*/
-
-/*//Добавление готовых карточек из массива в document через функции
-initialCards.forEach(item => {
-  createCardElement(item);
-  addCard(item);
-});*/
 
 //Функция вызова готового попапа по клику на изображение
 function handleCardClick(title, link) {
@@ -252,7 +100,7 @@ const popupAddCard = new PopupWithForm('#add-popup', {
       name: titleInput.value,
       link: urlInput.value
     };
-    addCard(newCard);
+    popupAddCard.addCard(newCard);
     popupAddCard.close();
   }
 });
---
+++ utils/constants.js
@@ -0,0 +1,78 @@
+// Импорт картинок
+const solnechnogorsk = new URL('../images/element_pic-4.jpg', import.meta.url);
+const kurshskayaKosa = new URL('../images/element_pic-5.jpg', import.meta.url);
+const vyborg = new URL('../images/element_pic-6.jpg', import.meta.url);
+
+//Исходный массив
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  },
+  {
+    name: 'Солнечногорск',
+    link: solnechnogorsk
+  },
+  {
+    name: 'Куршская коса',
+    link: kurshskayaKosa
+  },
+  {
+    name: 'Выборг',
+    link: vyborg
+  }
+];
+
+//Объект - конфигуратор со свойствами селекторов и классов
+const settings = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+};
+
+//Константы
+const elementsContainer = document.querySelector('.elements');
+const profileButtonEdit = document.querySelector('.profile__button-edit');
+const profileButtonAdd = document.querySelector('.profile__button-add');
+const userNameInput = document.querySelector('.popup__input_type_name');
+const userPositionInput = document.querySelector('.popup__input_type_position');
+const titleInput = document.querySelector('.popup__input_type_heading');
+const urlInput = document.querySelector('.popup__input_type_url');
+const cardSection = '.elements';
+
+export {
+  initialCards,
+  settings,
+  initialCards,
+  elementsContainer,
+  profileButtonAdd,
+  profileButtonEdit,
+  userNameInput,
+  userPositionInput,
+  titleInput,
+  urlInput,
+  cardSection
+};
